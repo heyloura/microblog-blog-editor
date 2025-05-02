a495e235ac206ee2c87493f1c6ccc430d2be3630
heyloura <112587406+heyloura@users.noreply.github.com>
2025-05-02 11:37:45 -0400

    fix loop on reset and logout

--- static/js/in-page-editor.js
+++ static/js/in-page-editor.js
@@ -2,20 +2,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const url = new URL(window.location.href);
     if(localStorage.getItem('in-page-editor-token') && url.searchParams.has('edit')) {
         // user has the token and the edit param
+        alert('welcome to edit mode!')
         // make content editable
         // add save button
         // add new button
     } else if(localStorage.getItem('in-page-editor-token') && url.searchParams.has('reset')) {
         // user has the token and wishes to reset it.
         promptSaveToken();
+        window.location.href.split("?")[0];
     } else if(localStorage.getItem('in-page-editor-token') && url.searchParams.has('logout')) {
         // user has the token and the edit param
         localStorage.setItem('in-page-editor-token', '');
         alert('Thank you for using my editor. Have a great day :-)');
-        window.location.reload();
+        window.location.href.split("?")[0];
     } else if (url.searchParams.has('edit')) {
         // prompt the user to save a micro.blog token
         promptSaveToken();
+        window.location.reload();
     } else {
         // don't do anything....
     }
@@ -25,7 +28,6 @@ function promptSaveToken() {
     let token = prompt("Please enter a Micro.blog token");
     if (token != null) {
         localStorage.setItem('in-page-editor-token', token);
-        window.location.reload();
     } else {
         alert('No token was saved.');
     }
