d18e55bf7022e230f5b01d296d168486793e1bf3
heyloura <112587406+heyloura@users.noreply.github.com>
2025-05-02 13:09:58 -0400

    first call to api endpoint

--- static/js/in-page-editor.js
+++ static/js/in-page-editor.js
@@ -1,23 +1,64 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     const url = new URL(window.location.href);
     if(localStorage.getItem('in-page-editor-token') && url.searchParams.has('edit')) {
         // user has the token and the edit param
         alert('welcome to edit mode!');
+        var NoURLException = {};
+        var NoHEntryException = {};
         // make content editable
-        let contents = document.querySelectorAll('.e-content');
-        contents = [...contents];
-        contents.forEach((content,i) => {
-            content.setAttribute('contenteditable','plaintext-only');
-            content.setAttribute('data-mpe-id',i);
-            content.addEventListener("focusout", (event) => {
-                event.target.innerHTML = event.target.innerText;
-            });
-            content.addEventListener("focusin", (event) => {
-                event.target.innerHTML = event.target.innerHTML.replaceAll('<','&lt;').trim();
-            });
-            content.insertAdjacentHtml('beforebegin','<button>save</button>')
-        });
+        try {
+            let fetching = await fetch(`https://able-hawk-60.deno.dev`, { method: "GET", headers: { "Authorization": "Bearer " + localStorage.getItem('in-page-editor-token') } } );
+            const results = await fetching.json();
 
+            let contents = document.querySelectorAll('.e-content');
+            contents = [...contents];
+            contents.forEach((content,i) => {
+                let hentry = content.closest('.h-entry');
+                if(!hentry) {
+                    throw NoHEntryException;
+                }
+                let url = hentry.querySelector('.u-url');
+                if(!url || !url.getAttribute('href')) {
+                    throw NoURLException;
+                } else {
+                    let name = hentry.querySelector('.p-name');
+
+                    if(name) {
+                        name.setAttribute('contenteditable','plaintext-only');
+                        name.setAttribute('data-mpe-id',i);
+                    }
+
+                    content.setAttribute('contenteditable','plaintext-only');
+                    content.setAttribute('data-mpe-id',i);
+                    content.addEventListener("focusout", (event) => {
+                        event.target.innerHTML = event.target.innerText;
+                    });
+                    content.addEventListener("focusin", (event) => {
+                        event.target.innerHTML = event.target.innerHTML.replaceAll('<','&lt;').trim();
+                    });
+                    hentry.insertAdjacentHTML('beforebegin',`<form>
+                        ${results.categories && results.categories.length > 0 ? 
+                            `<fieldset>
+                                <legend>Post Categories</legend>
+                                ${results.categories.map(item => {
+                                    return `<label>
+                                            <input type="checkbox" ${categoriesParam.map(function (s) { return s == item ? 'checked="checked"' : '' }).join('')} name="category[]" value="${item}"> ${item}
+                                            </label>`;
+                                }).join('')}
+                            </fieldset>` : '' }
+                            <button type="button">save</button>
+                        </form>`);
+                }
+            });
+        } catch(e) {
+            if(e === NoURLException) {
+                alert('No u-url found for content.');
+            } else if(e === NoHEntryException) {
+                alert('No h-entry found for content.');
+            } else { 
+                throw e; 
+            }
+        }
         // add save button
         // add new button
     } else if(localStorage.getItem('in-page-editor-token') && url.searchParams.has('reset')) {
